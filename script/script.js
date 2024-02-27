12653a8d77ce28b45ab69c42167a420a702987f0
seitmohaideen <161434234+seitmohaideen@users.noreply.github.com>
2024-02-28 02:28:38 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -19,6 +19,14 @@ navLinks.forEach((l) => {
 })
 
 /*seit Added*/
-(function($){
-        new WOW().init();
-    })(jQuery);
+document.addEventListener('DOMContentLoaded', function() {
+    const skillBars = document.querySelectorAll('.skill-level-bar');
+    
+    skillBars.forEach(skillBar => {
+        const skillPercent = skillBar.getAttribute('data-percent');
+        skillBar.style.width = skillPercent;
+        // Triggering the animation by accessing the offsetWidth
+        skillBar.offsetWidth;
+        skillBar.style.width = skillPercent;
+    });
+});
